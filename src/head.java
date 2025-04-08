06927d85efb5eb9641741d5433e5f3cb99a9e9d6
Ethan Meng <ethan@rapidcow.org>
2025-04-08 01:22:44 -0500

    Release old thingy too
    i hope i didn't mess up any more typos again :x

--- src/head.java
+++ src/head.java
@@ -1,5 +1,5 @@
     //
-    // This is part of TAP for Java, version unreleased.
+    // This is part of TAP for Java, version 0.1.6_00.
     // Copyrigh (c) 2025 Ethan Meng <ethan@rapidcow.org>
     //
     /*=================== BEGIN TAP FOR JAVA ===================*/
